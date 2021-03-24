44667e00312e844c3bd0c569ae71bb4d872ac38a
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-03-24 21:12:54 +0100

    Add production deployment script

---
+++ scripts/production-gatsby-deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Preload base bash configuration and functions
+source scripts/base.sh
+setup_base_config
+
+info "Environment: production"
+
+GATSBY_ARCHIVE_PATH=$1
+
+validate_non_empty $GATSBY_ARCHIVE_PATH "GATSBY_ARCHIVE_PATH"
+check_if_file_exists $GATSBY_ARCHIVE_PATH
+
+rsync -azP --delete public/ staging:/var/www/blog
+scp $GATSBY_ARCHIVE_PATH staging:/etc/backups/blog
