263b308c89dc4981d2815342fb4e62e6c9897354
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-06-07 20:08:59 +0200

    step: add -f option to the local-gatsby-start script

--- scripts/local-gatsby-start.sh
+++ scripts/local-gatsby-start.sh
@@ -6,4 +6,20 @@ setup_base_config
 
 info "Environment: local"
 
+SHOULD_CLEAR_GATSBY_CACHE=0
+
+while test "$#" -gt 0; do
+    case $1 in
+        -f|--force) SHOULD_CLEAR_GATSBY_CACHE=1;;
+        *) echo "Unknown parameter passed: $1"; exit 1;;
+    esac
+    shift
+done
+
+if test $SHOULD_CLEAR_GATSBY_CACHE == 1;
+then
+  info "Clearing Gatsby cache..."
+  gatsby clean
+fi
+
 npx gatsby develop
